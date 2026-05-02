f642d9d3e8f7f584d14bc4d12c3900e424bb990a
Jordan Roberts <81574252+JordanRoberts-2000@users.noreply.github.com>
2026-05-02 21:10:51 +0100

    feat: add basic PWA support (manifest + service worker)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      manifest: {
+        name: "API Display",
+        theme_color: "#fef9c2",
+        background_color: "#fef9c2",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:3004",
